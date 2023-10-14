890567a3226d81876673385a311fb47bf27fc339
TheSaviourEking <saviour.eking@outlook.com>
2023-10-14 04:15:22 +0100

    SOLVED: add solution for 03-queue.js

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,22 +5,48 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val);
+        if (!this.head && !this.tail) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
 
+        return ++this.length;
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
-        
+        if (!this.head) return null;
+        const head = this.head;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = this.head;
+        } else {
+            this.head = this.head.next;
+        }
+        return head.value;
         // Write your hypothesis on the time complexity of this method here
     }
 
 }
 
+const queue = new Queue();
+queue.enqueue('A');
+queue.enqueue('B');
+// queue.enqueue('C');
+console.log(queue.head.next.value);
+// console.log(queue)
 module.exports = {
     Queue,
     SinglyLinkedNode
