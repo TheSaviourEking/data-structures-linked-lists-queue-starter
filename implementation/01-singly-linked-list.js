c019a4ed54b1c98d4a7df3647e3b6f7a9f69ecb3
Saviour Eking <saviour.eking@outlook.com>
2023-10-18 05:33:39 +0100

    SOLVED: add solution for 01-singly-linked-list.js

--- implementation/01-singly-linked-list.js
+++ implementation/01-singly-linked-list.js
@@ -14,24 +14,22 @@ class SinglyLinkedList {
 
     addToHead(val) {
         // Add node of val to head of linked list
-        let newNode = new SinglyLinkedNode(val);
-        if (!this.head) this.head = newNode;
-        else {
-            newNode.next = this.head;
-            this.head = newNode;
-        }
+        const newNode = new SinglyLinkedNode(val);
+        newNode.next = this.head;
+        this.head = newNode;
         this.length++;
         return this;
 
         // Write your hypothesis on the time complexity of this method here
+        // this works in O(1) time and O(n) space;
     }
 
     addToTail(val) {
         // There are bugs in this method! Fix them!!!
         // Write your hypothesis on the time complexity of this method here
+        // works in O(n) times because it depends on the length of the linked list
 
         // Add node of val to tail of linked list
-        // let newNode = new SinglyLinkedNode(data);
         let newNode = new SinglyLinkedNode(val);
 
         if (!this.head) {
@@ -50,54 +48,54 @@ class SinglyLinkedList {
 
     removeFromHead() {
         // Remove node at head
-        if (!this.head) return undefined;
-        else if (this.length === 1) {
+        if (this.length <= 0) return undefined;
+        const removedHead = this.head;
+        if (this.length === 1) {
             this.head = null;
-            this.length--;
-        }
-        else {
-            let head = this.head;
+        } else {
             this.head = this.head.next;
-            this.length--;
-            return head;
         }
 
+        this.length--;
+        return removedHead;
+
         // Write your hypothesis on the time complexity of this method here
+        // work in O(1) time complexity
     }
 
     removeFromTail() {
         // Remove node at tail
-        if (!this.head) return undefined;
-        else if (this.length === 1) {
+        if (this.length <= 0) return undefined;
+        if (this.length === 1) {
             let tail = this.head;
             this.head = null;
             this.length--;
             return tail;
-        } else {
-            let current = this.head;
-            while (current.next.next !== null) {
-                current = current.next;
-            }
-            let tail = current.next;
-            current.next = null;
-            this.length--;
-            return tail;
         }
+        let tail = this.head;
+        while (tail.next.next) {
+            tail = tail.next;
+        }
+        let removedTail = tail.next;
+        tail.next = null;
+        this.length--;
+        return removedTail;
 
         // Write your hypothesis on the time complexity of this method here
+        // works in O(n) time complexity
     }
 
     peekAtHead() {
         // Return value of head node
-        if (!this.head) return undefined;
+        if (this.length <= 0) return undefined;
         return this.head.value;
 
         // Write your hypothesis on the time complexity of this method here
+        // works in O(1) time complexity
     }
 
     print() {
         // Print out the linked list
-        if (!this.head) return;
         let current = this.head;
         while (current) {
             console.log(current.value);
@@ -105,6 +103,7 @@ class SinglyLinkedList {
         }
 
         // Write your hypothesis on the time complexity of this method here
+        // works in O(n) time complexity
     }
 }
 
